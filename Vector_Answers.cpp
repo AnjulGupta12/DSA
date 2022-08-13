af11d45fd37b5374a876690f382819d483228575
Anjul Gupta <76518288+AnjulGupta12@users.noreply.github.com>
2022-08-13 21:00:49 +0530

    Update Vector_Answers.cpp

--- Vector_Answers.cpp
+++ Vector_Answers.cpp
@@ -242,7 +242,28 @@ Constraints:
 -30 <= nums[i] <= 30
 The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 */
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> rightProd(n,1);
+        vector<int> leftProd(n, 1 );
+        
+        for(int i = n-2; i>=0; i--)
+        {rightProd[i] = rightProd[i+1]*nums[i+1];}
+        for(int i = 1; i<n; i++)
+        {leftProd[i] = leftProd[i-1]*nums[i-1];}
+        for(int i =0; i<n; i++)
+        {nums[i]=rightProd[i]*leftProd[i];}
+        
+        return nums;
+    }
+};
 
+
+
+
+//
 
 
 
